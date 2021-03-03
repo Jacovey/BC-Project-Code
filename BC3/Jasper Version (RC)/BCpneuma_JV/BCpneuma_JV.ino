b4f91a3e547f3b9cdd7fcdc12447c4e66c81a149
Regred002 <jaspercovey@gmail.com>
2021-03-03 12:34:04 -0700

    3 pneuma

--- BC3/Jasper Version (RC)/BCpneuma_JV/BCpneuma_JV.ino
+++ BC3/Jasper Version (RC)/BCpneuma_JV/BCpneuma_JV.ino
@@ -5,20 +5,18 @@
 // i2c slave (address 1) receiving input from nano/LoRa radio (address 2)
 
 /*
- 
- NEW RC-STYLE MULTICHANNEL IMPLEMENTATION
+ RC-STYLE MULTICHANNEL IMPLEMENTATION
 
- CONTROL MAPPING:
+ CONTROL MAPPING FOR 3:
  ORDER    : L1 L2 L3 L4  S  A  C  P  B  BL  MA
  Channel  :  1  2  3  4  5  6  7  8  9  10  11
  Paddress : 22 24 26 28 30 32 34 36 38  50  42/45
  Vaddress : 23 25 27 29 31 33 35 37 39 N/A  44/43
  Psensor  : A0 A1 A3    A4 A5 A6    A8
 
-  NOTES FOR BC3: 
-  A0 and A2 pressure sensors are dead, swapped L3 to A3 and L1 to A7. 
-  No Feedback on pig or L4 as of 3/1/2020.
-
+ NOTES FOR BC3: 
+ A2 pressure sensors is dead, swapped L3 to A3
+ No Feedback on pig or L4 as of 3/1/2020.
 */
 
 #define numChan 11
@@ -27,7 +25,7 @@
 #define MAXPRESSURE 14.5
                                         //SEE NOTES FOR A2-A3 SWAP
 static int numValveChan = numChan-2;
-static int pSensPins[numChan-2]           = {A0, A1, A2, A3, A4, A5, A6, A7, A8};  // A9, A10, A11,  A12,  A13,  A14,  A15};
+static int pSensPins[numChan-2]           = {A0, A1, A3, A3, A4, A5, A6, A7, A8};  // A9, A10, A11,  A12,  A13,  A14,  A15};
 static unsigned int pValvePins[numChan-2] = {22, 24, 26, 28, 30, 32, 34, 36, 38};  // 40, 42,  44,   46,   48,   50,   52};
 static unsigned int vValvePins[numChan-2] = {23, 25, 27, 29, 31, 33, 35, 37, 39};  // 41, 43,  45,   47,   49,   51,   53};
 static unsigned int masterPressurePins[2] = {42, 45};
@@ -40,17 +38,19 @@ int pressureErrors[numChan-2]   =           {0,  0,  0,  0,  0,  0,  0,  0,  0};
 uint8_t data[numChan]           =                       {0,0,0,0,0,0,0,0,0,0,0};
 uint8_t LEDdata[numChan-2]      =                           {0,0,0,0,0,0,0,0,0};
 
-//DEBUGGING
-static bool debug = true;
-
 void setup() {
+  //Start Serial
   Serial.begin(9600);
+
+  //Initiliaze each valve pin
   for (int i = 0; i < numValveChan; i++) {
     pinMode(pValvePins[i], OUTPUT);
     pinMode(vValvePins[i], OUTPUT);
     digitalWrite(vValvePins[i], LOW);
     digitalWrite(pValvePins[i], LOW);    
   }
+
+  //Initialize the master valves
   for (int i = 0; i < 2; i++) {
     pinMode(masterVacuumPins[i], OUTPUT);
     pinMode(masterPressurePins[i], OUTPUT);
@@ -58,10 +58,11 @@ void setup() {
     digitalWrite(masterPressurePins[i], LOW);
   }
 
-  //Listen to wire 1 for Serial communications
+  // Listen to wire 1 for Serial communications
   Wire.begin(1);
   Wire.onReceive(receiveCommand);
 
+  // Init Message
   Serial.println("Border Crosser 5 Ready.");
   Serial.println();
 
@@ -79,41 +80,52 @@ void receiveCommand(int howmany) {
 }
  
 void loop() {
-  static unsigned long last_display_time = 0;
+  static unsigned long last_display_time = 0; // to keep track of time since last display update
   for (int i = 0; i<numValveChan; i++) {
     //Pressure Sensor Interpreting
     currentPressures[i] = analogRead(pSensPins[i]);
-    currentPressures[i] = constrain(currentPressures[i], 41, 962); // keeps in range of 0-14.5 PSI (one bar)
-    currentPressures[i] = ((((currentPressures[i] / 1023. * 5.0) - 0.2 ) / 4.5 * 1023.) / (MAXPRESSURE * 70.551727)) * 127; //0.2 Volts = 0psi 4.7 Volts = 14.5psi 4.5 Volts = Span of 0-14.5psi.  turned to 7 bit value
+    currentPressures[i] = constrain(currentPressures[i], 41, 962); // keeps in range of 0-MAXPRESSURE in PSI
+    currentPressures[i] = ((((currentPressures[i] / 1023. * 5.0) - 0.2 ) / 4.5 * 1023.) / (MAXPRESSURE * 70.551727)) * 127; //Returns 0-MAXPRESSURE
     
     //Handling Proportional Drive
     if (servoOverride[i] == 0) {  // if it's NOT in manual direct drive mode
+      //get the errors
       pressureErrors[i] = (targetPressures[i]) - (currentPressures[i]);
       if (abs(pressureErrors[i]) < PRESSURE_TOLERANCE) { // IDLE
+        // stop flow in and out of link
         digitalWrite(pValvePins[i], LOW);
         digitalWrite(vValvePins[i], LOW);
+        // if the pressure is within tolerance, make the LED green
         LEDdata[i] = 1;
       }
       else if (pressureErrors[i] > 0) { // PRESSURIZE
+        // stop flow out, start flow in
         digitalWrite(pValvePins[i], HIGH);
         digitalWrite(vValvePins[i], LOW);
+        // if the pressure is outside of tolerance, make the LED red
         LEDdata[i] = 0;
       }
       else if (pressureErrors[i] < 0) { // VENT
+        // stop flow in, start flow out
         digitalWrite(pValvePins[i], LOW);
         digitalWrite(vValvePins[i], HIGH);
+        // if the pressure is outside of tolerance, make the LED red
         LEDdata[i] = 0;
       }
-    } // updating the LEDs for manual drive
+    } 
+    // Updating the LEDs for manual drive
     else{
       //get commanded state
       int state = data[i];
 
       // Update LED's based on condition
-      if (state == 104) LEDdata[i]=2; //Manual idle--> light fully off
-      else if (state == 102 && currentPressures[i]<MAXPRESSURE-2*PRESSURE_TOLERANCE) LEDdata[i]=0;
-      else if (state == 102)                                                               LEDdata[i]=4;
-      else if (state == 101)                                                               LEDdata[i]=3;
+      // if manual idle, turn off light
+      if (state == 104) LEDdata[i]=2; 
+      // if the link is in manual pressurize, make the LED red until you get to max pressure, then blink green
+      else if (state == 102 && currentPressures[i]<MAXPRESSURE-2*PRESSURE_TOLERANCE) LEDdata[i]=0; 
+      else if (state == 102)                                                         LEDdata[i]=4;
+      // if the link is in manual vent, blink red
+      else if (state == 101)                                                         LEDdata[i]=3;
     }
   }
   
@@ -129,13 +141,16 @@ void loop() {
 }
 
 void handleValve(byte channel, byte state) {
+  //KEY: 101 is "on"/"pressurize", 102 is "off"/"depressurize", 103 is a switch error, 104 is "idle"
+  //     any other value is read as a value from 0-100 inclusive
+  
   // Blower Control
-  if (channel == 9){
+  if (channel == numChan-2){
     if  (state == 102) {digitalWrite(blowerRelay, LOW);}  //Serial.println("BLOWER OFF");}
     if  (state == 101) {digitalWrite(blowerRelay, HIGH);} //Serial.println("BLOWER ON");}
   }
   // Master Pressure Control
-  else if (channel == 10){
+  else if (channel == numChan-1){
     if  (state == 101){
       //Serial.println("MASTER VACUUM ON");
       for (int i = 0; i < 2; i ++){
